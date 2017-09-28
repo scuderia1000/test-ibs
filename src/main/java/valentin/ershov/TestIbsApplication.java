c59a4e76211c2dcd43770cb8dc57c45511d21bfb
scuderia1000 <a4cbepk@gmail.com>
2017-09-28 18:44:56 +0500

    Initial commit

---
+++ src/main/java/valentin/ershov/TestIbsApplication.java
@@ -0,0 +1,13 @@
+package valentin.ershov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestIbsApplication.class, args);
+	}
+}
